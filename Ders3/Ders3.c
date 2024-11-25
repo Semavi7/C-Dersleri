666764c38a8ee65fd7ffdf60f93c0ba5519c52d9
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-11-25 03:21:51 +0300

    Ders3

--- Ders3/Ders3.c
+++ Ders3/Ders3.c
@@ -3,10 +3,25 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
+//%.1f yapýnca virgülden sonraki tek rakamý yazýyor.
 int main() {
 	setlocale(LC_ALL, "Turkish");
 
+    int a, b, toplama1, cýkarma1;
+    float n, z, toplama2, cýkarma2;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%f", &n);
+    scanf("%f", &z);
+
+    toplama1 = a + b;
+    cýkarma1 = a - b;
+    toplama2 = n + z;
+    cýkarma2 = n - z;
+
+    printf("%d %d\n", toplama1, cýkarma1);
+    printf("%.1f %.1f", toplama2, cýkarma2);
 
 
 
