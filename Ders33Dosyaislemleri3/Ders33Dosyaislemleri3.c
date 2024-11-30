93984b6817a3bb1bd88f8e8fe1ba84a6efa08caf
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-11-30 22:16:09 +0300

    Ders33

--- Ders33Dosyaislemleri3/Ders33Dosyaislemleri3.c
+++ Ders33Dosyaislemleri3/Ders33Dosyaislemleri3.c
@@ -3,32 +3,22 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
 int main() {
-	setlocale(LC_ALL, "Turkish");
+setlocale(LC_ALL, "Turkish");
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+	FILE* dosya;
+	dosya = fopen("C:\\Users\\semav\\OneDrive\\Desktop\\yeni3.txt", "w");
 
+	fputs("Mehmet Burçhan Gürses", dosya);
+	putc('\n', dosya);
+	fputs("38", dosya);
+	putc('\n', dosya);
+	fputs("Anadolu Universitesi", dosya);
 
-	int main() {
-		setlocale(LC_ALL, "Turkish");
-
-		FILE* dosya;
-		dosya = fopen("C:\\Users\\semav\\OneDrive\\Desktop\\yeni2.txt", "w");
-
-		putc('a', dosya);
-		putc('\n', dosya);
-		putc('b', dosya);
-
-		fclose(dosya);
+	fclose(dosya);
 
 
 		return 0;
 	}
 
 
-	return 0;
-}
